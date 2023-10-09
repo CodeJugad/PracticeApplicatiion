ce40b15685f0b766c75226db912356bc4e8b896b
CodeJugad <y314yadav@gmail.com>
2023-10-09 11:19:51 +0530

    using material design basic level

--- app/src/main/java/com/example/practiceapplicatiion/MainActivity.kt
+++ app/src/main/java/com/example/practiceapplicatiion/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.practiceapplicatiion
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +13,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fragment = BlankFragment()
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.myFragment, fragment)
-            .commit()
+        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottom_app_bar)
+        setSupportActionBar(bottomAppBar)
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { // Handle FAB click event
+            Toast.makeText(this@MainActivity, "FAB Clicked", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
+
+
+
